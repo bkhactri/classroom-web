ec5d98440605f7242a294b20f8199761525b9d44
vohathnhbinh <56076993+vohathnhbinh@users.noreply.github.com>
2021-12-01 17:06:30 +0700

    Add grade structure feature (#7)

--- src/pages/ClassroomPeople/ClassroomPeople.js
+++ src/pages/ClassroomPeople/ClassroomPeople.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Container from "@mui/material/Container";
-import Snackbar from '@mui/material/Snackbar';
+import Snackbar from "@mui/material/Snackbar";
 
 import UserLogo from "../../assets/images/user-logo.png";
 import classes from "./ClassroomPeople.module.css";
@@ -21,11 +21,13 @@ const ClassroomPeople = () => {
     const fetchParticipants = async () => {
       setIsLoading(true);
       try {
-        const result = await axiosClassroom.get(`/participants?classroomID=${classroomId}`, {
-          headers: { Authorization: "Bearer " + accessToken },
-        });
+        const result = await axiosClassroom.get(
+          `/participants?classroomID=${classroomId}`,
+          {
+            headers: { Authorization: "Bearer " + accessToken },
+          }
+        );
         setParticipants(result.data);
-        console.log(result.data)
         setIsLoading(false);
       } catch (error) {
         setIsLoading(false);
@@ -39,57 +41,66 @@ const ClassroomPeople = () => {
   const handleCloseSnackBar = () => setSnackBarMessage("");
 
   const getNumStudent = () => {
-    return participants.filter((participant) => participant["role"] === "STUDENT").length;
-  }
+    return participants.filter(
+      (participant) => participant["role"] === "STUDENT"
+    ).length;
+  };
 
   const participantList = (roles) => {
-    if (!isLoading){
-      return participants.map((participant) => {
-        if (roles.includes(participant["role"])){
-          return(
-            <div key={participant["userID"]}>
-              <ListItem key={participant["userID"]}>
-                <img className={classes.avatar} src={UserLogo} alt="avatar"/>
-                <Typography variant="p" className={classes.studentName}>{participant["user"]["username"]}</Typography>
+    if (!isLoading) {
+      return participants
+        .filter((participant) => roles.includes(participant["role"]))
+        .map((participant) => {
+          return (
+            <div key={participant["userId"]}>
+              <ListItem>
+                <img className={classes.avatar} src={UserLogo} alt="avatar" />
+                <Typography variant="p" className={classes.studentName}>
+                  {participant["user"]["username"]}
+                </Typography>
               </ListItem>
-              <Divider/>                  
+              <Divider />
             </div>
-          )             
-        };
-        return null;
-      })
-    } 
+          );
+        });
+    }
     return null;
-  }
+  };
 
   return (
-    <>  
+    <>
       <Snackbar
         open={Boolean(snackBarMessage)}
         autoHideDuration={5000}
         onClose={handleCloseSnackBar}
         message={snackBarMessage}
       />
-      <Header loading={isLoading} classroom={2} classID={classroomId}/>
+      <Header loading={isLoading} classroom={2} classID={classroomId} />
       <Container classes={{ root: classes.classroomPeopleContainer }}>
-
         <List>
           <ListItem divider>
-            <Typography variant="h4" className={classes.bigfont}>Teachers</Typography>
+            <Typography variant="h4" className={classes.bigfont}>
+              Teachers
+            </Typography>
           </ListItem>
-          <Divider className={classes.dividerBlue}/>
+          <Divider className={classes.dividerBlue} />
 
           {participantList(["OWNER", "TEACHER"])}
-          
         </List>
 
-
         <List>
-          <ListItem style={{display:'flex', justifyContent:'space-between'}} divider>
-            <Typography variant="h4" className={classes.bigfont}>Classmates</Typography>
-            <Typography variant="p" className={classes.fontNumClass}>{getNumStudent()} students</Typography>
+          <ListItem
+            style={{ display: "flex", justifyContent: "space-between" }}
+            divider
+          >
+            <Typography variant="h4" className={classes.bigfont}>
+              Classmates
+            </Typography>
+            <Typography variant="p" className={classes.fontNumClass}>
+              {getNumStudent()} students
+            </Typography>
           </ListItem>
-          <Divider className={classes.dividerBlue}/>
+          <Divider className={classes.dividerBlue} />
 
           {participantList(["STUDENT"])}
         </List>
---
+++ src/pages/GradeStructure/GradeStructure.js
@@ -0,0 +1,304 @@
+import { Fragment, React, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Header from "../../components/Header/Header";
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Collapse from "@mui/material/Collapse";
+import Alert from "@mui/material/Alert";
+import CircularProgress from '@mui/material/CircularProgress';
+import Fab from "@mui/material/Fab";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
+import Grade from "../../components/Grade/Grade";
+
+import axiosClassroom from "../../api/classroom.axios";
+
+const GradeStructure = (props) => {
+  const accessToken = useSelector((state) => state.auth.token);
+  const history = useHistory();
+  const { classroomId } = useParams();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [grades, setGrades] = useState([]);
+  const [inputName, setInputName] = useState("");
+  const [inputPoint, setInputPoint] = useState("");
+  const [errorMessages, setErrorMessages] = useState([]);
+  const [timer, setTimer] = useState(null);
+
+  useEffect(() => {
+    const fetchClassroom = async () => {
+      setIsLoading(true);
+      try {
+        const result = await axiosClassroom.get(`/${classroomId}`, {
+          headers: { Authorization: "Bearer " + accessToken },
+        });
+        setGrades(result.data.grades || []);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+      }
+    };
+
+    fetchClassroom();
+  }, [accessToken, classroomId]);
+
+  const savingGrades = async (latestGrades) => {
+    return await axiosClassroom.put(
+      `/${classroomId}`,
+      { grades: latestGrades },
+      { headers: { Authorization: "Bearer " + accessToken } }
+    );
+  };
+
+  const handleNameChange = (event) => {
+    setInputName(event.target.value);
+  };
+
+  const handlePointChange = (event) => {
+    setInputPoint(event.target.value);
+  };
+
+  const addGrade = () => {
+    let invalid = false;
+    clearTimeout(timer);
+    setErrorMessages([]);
+
+    if (grades.find((grade) => inputName === grade.name)) {
+      setErrorMessages(
+        errorMessages.concat("A grade with that name already exist")
+      );
+      invalid = true;
+    }
+    if (!inputName) {
+      setErrorMessages(errorMessages.concat("Name is required"));
+      invalid = true;
+    }
+    if (!inputPoint) {
+      setErrorMessages(errorMessages.concat("Point is required"));
+      invalid = true;
+    }
+    if (isNaN(inputPoint)) {
+      setErrorMessages(errorMessages.concat("Point must be a number"));
+      invalid = true;
+    }
+
+    if (!invalid) {
+      setIsLoading(true);
+
+      const newGrades = grades.concat({ name: inputName, point: inputPoint });
+      setGrades(newGrades);
+      setInputName("");
+      setInputPoint("");
+
+      savingGrades(newGrades).finally(() => setIsLoading(false));
+    }
+
+    setTimer(
+      setTimeout(() => {
+        setErrorMessages([]);
+      }, 5000)
+    );
+  };
+
+  const saveGrade = (originalName, newGrade) => {
+    setIsLoading(true);
+
+    const newGrades = grades.map((grade) => {
+      return originalName === grade.name ? newGrade : grade;
+    });
+    setGrades(newGrades);
+
+    savingGrades(newGrades).finally(() => setIsLoading(false));
+  };
+
+  const deleteGrade = (name) => {
+    setIsLoading(true);
+
+    const newGrades = grades.filter((grade) => name !== grade.name);
+    setGrades(newGrades);
+
+    savingGrades(newGrades).finally(() => setIsLoading(false));
+  };
+
+  const saveOrder = () => {
+    setIsLoading(true);
+
+    savingGrades(grades).finally(() => setIsLoading(false));
+  };
+
+  const onDragEnd = (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const reorder = (list, startIndex, endIndex) => {
+      const result = Array.from(list);
+      const [removed] = result.splice(startIndex, 1);
+      result.splice(endIndex, 0, removed);
+
+      return result;
+    };
+
+    const items = reorder(
+      grades,
+      result.source.index,
+      result.destination.index
+    );
+
+    setGrades(items);
+  };
+
+  return (
+    <>
+      <Header loading={isLoading} />
+      <Container component={Card} maxWidth="sm" sx={{ mt: 5 }}>
+        <Box
+          sx={{
+            m: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Fab
+            color="primary"
+            title="Back to classroom"
+            aria-label="back"
+            sx={{ mb: 5 }}
+            onClick={() => history.goBack()}
+          >
+            <ArrowUpwardIcon />
+          </Fab>
+          <Typography variant="h5" mb={4}>
+            Grade Structure
+          </Typography>
+
+          <Grid container>
+            <Grid item xs={8}>
+              <Typography variant="h5" color="blue">
+                Name
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography variant="h5" color="crimson">
+                Point
+              </Typography>
+            </Grid>
+            {grades.map((grade) => (
+              <Fragment key={grade.name}>
+                <Grid item xs={8}>
+                  <Typography variant="h6">{grade.name}</Typography>
+                </Grid>
+                <Grid item>
+                  <Typography variant="h6">{grade.point}</Typography>
+                </Grid>
+              </Fragment>
+            ))}
+          </Grid>
+          <Button
+            variant="contained"
+            fullWidth
+            sx={{ mt: 2 }}
+            onClick={saveOrder}
+            disabled={isLoading}
+          >
+            {isLoading && <CircularProgress color="success" size={25} />}
+            &nbsp;
+            Save Order
+          </Button>
+        </Box>
+      </Container>
+
+      <Container component={Card} maxWidth="sm" sx={{ mt: 1 }}>
+        <Box
+          sx={{
+            m: 3,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Collapse in={errorMessages.length !== 0}>
+            {errorMessages.map((error, index) => (
+              <Alert
+                key={index}
+                severity="error"
+                variant="filled"
+                sx={{ my: 1 }}
+              >
+                {error}
+              </Alert>
+            ))}
+          </Collapse>
+
+          <TextField
+            fullWidth
+            label="Name"
+            variant="standard"
+            value={inputName}
+            onChange={handleNameChange}
+          />
+
+          <TextField
+            fullWidth
+            label="Point"
+            variant="standard"
+            value={inputPoint}
+            onChange={handlePointChange}
+            sx={{ mt: 1 }}
+          />
+          <Button
+            onClick={addGrade}
+            disabled={!inputName || !inputPoint || isLoading}
+          >
+            Add
+          </Button>
+        </Box>
+      </Container>
+
+      {grades.length > 0 && (
+        <Container component={Card} maxWidth="sm" sx={{ mt: 1, mb: 5 }}>
+          <Box
+            sx={{
+              m: 5,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided, snapshot) => (
+                  <div {...provided.droppableProps} ref={provided.innerRef}>
+                    {grades.map((grade, index) => (
+                      <Grade
+                        key={grade.name}
+                        grade={grade}
+                        index={index}
+                        saveGrade={saveGrade}
+                        deleteGrade={deleteGrade}
+                        isLoading={isLoading}
+                      />
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </DragDropContext>
+          </Box>
+        </Container>
+      )}
+    </>
+  );
+};
+
+export default GradeStructure;
